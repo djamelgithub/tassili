1b9ae5ddfa2de82ec74c6c5b5388a0a0fd51bc85
Djamel <pruevadjamel2020@hotmail.com>
2023-08-27 00:29:56 +0000

    Mensaje del commit

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -50,19 +50,7 @@ const userCtrl = {
               return res.status(500).json({ msg: err.message });
             }
           },
-          
-   /*
    
-    searchUser: async (req, res) => {
-        try {
-            const users = await Users.find({username: {$regex: req.query.username}})
-            .limit(10).select("fullname username avatar")
-            
-            res.json({users})
-        } catch (err) {
-            return res.status(500).json({msg: err.message})
-        }
-    },*/
     getUser: async (req, res) => {
         try {
             const user = await Users.findById(req.params.id).select('-password')
--- routes/postvistasrouter.js
+++ routes/postvistasrouter.js
@@ -1,9 +1,8 @@
 const router = require('express').Router()
 const postvistasCtrl = require('../controllers/postvistasCtrl')
-//const postvistasCtrl = require('../controllers/postvistasCtrl')
+ 
 const auth = require('../middleware/auth')
  
-//router.put('/post/vistas/:id', auth, postCtrl.getVistas);
  
 router.put('/post/:id', auth, postvistasCtrl.Vistas)
 
